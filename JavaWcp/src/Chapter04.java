fa1a1aa439c7742a67684e941c59850c0bcd6cdb
nkmry <yuuun.k16@gmail.com>
2022-02-09 16:51:40 +0900

    [Add]Chapter04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "Nakamura";
+		final int NUMBER = 8;
+		System.out.println(name);
+		name = "第３者への態度がその人の真の性格";
+		System.out.println(name);
+		System.out.println(NUMBER);
+	}
+}
