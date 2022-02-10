f52e14c254e9ad25026c5733a0be103f71b5fea3
nkmry <yuuun.k16@gmail.com>
2022-02-10 16:24:54 +0900

    [Add]Chapter08

---
+++ JavaWcp/src/Chapter08.java
@@ -0,0 +1,26 @@
+
+public class Chapter08 {
+	public static void main(String[] args) {
+		
+//		While文
+		int number = 1;
+		while (number < 5) {
+			System.out.println(number * number);
+			number++;
+		}
+		
+//		通常for文
+		int[] array = { 1, 21, 33, 48 };
+		for (int i = 1; i <= array.length; i++) {
+			System.out.println(i);
+		}
+		
+//		拡張for文
+		for (int val : array) {
+			if (val % 2 == 0) {
+				continue;
+			} 
+			System.out.println(val);
+		}
+	}
+}
