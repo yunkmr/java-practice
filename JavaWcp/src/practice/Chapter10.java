4abadea8636a53f6ca235389760e5f0c6b92204f
nkmry <yuuun.k16@gmail.com>
2022-02-12 16:08:44 +0900

    [Add]Chapter10

--- JavaWcp/src/practice/Chapter10.java
+++ JavaWcp/src/practice/Chapter10.java
@@ -15,26 +15,36 @@ public class Chapter10 {
 			System.out.println("IllegalArgumentExceptionが発生しました");
 			e.printStackTrace();
 		}
+		
+        // try-catch文を記述
+        // ・throwSQLExceptionメソッドを呼び出し
+        // ・SQLExceptionをキャッチして、メッセージとスタックトレースを出力
+        // ・"throwSQLExceptionの呼び出し終了"のメッセージを出力
         try {
             Chapter10.throwSQLException();
         } catch (SQLException e) {
             System.out.println("SQLExceptionが発生しました");
             e.printStackTrace();
         } finally {
+        	// 例外発生しても出力するため、finallyブロックに記述
             System.out.println("throwSQLExceptionの呼び出し終了");
         }
         
         System.out.println("mainメソッド終了");
 	}
 	
+    // validIndexメソッドを作成
+    // ・indexがarrayのサイズの範囲内なら、インデックスの要素を出力
+    // ・サイズの範囲外なら、IllegalArgumentExceptionをスロー
     public static void validIndex(int[] array, int index) {
         if (array.length <= index) {
             throw new IllegalArgumentException(index + " はサイズの範囲外です");
         }
         System.out.println("インデックス " + index + " の要素は " + array[index] + " です");
     }
-    
+ // スローされる例外が検査例外なので、throws文が必要
     public static void throwSQLException() throws SQLException {
+    	// SQLExceptionは、通常、SQLでエラーの場合に発生する例外
         throw new SQLException("SQLエラーです");
     }
 }
