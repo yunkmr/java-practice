9258998bd314f66f93e05364c1397bbdd60fbec8
nkmry <yuuun.k16@gmail.com>
2022-02-16 10:05:30 +0900

    [Add]Chapter13

---
+++ JavaWcp/src/practice/MobilePhone.java
@@ -0,0 +1,20 @@
+package practice;
+
+import extend.phone.Camera;
+import extend.phone.Phone;
+
+public class MobilePhone implements Phone, Camera {
+	private String number;
+	
+	public MobilePhone(String number) {
+		this.number = number;
+	}
+	
+	public void takePicture() {
+		System.out.println("写真をとります。");
+	}
+	
+	public void call(String number) {
+		System.out.println(this.number + " から " + number + " に電話をかけます。");
+	}
+}
