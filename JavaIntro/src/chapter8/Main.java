8130b0326118834276a2d6f2128c8cb52a1ccfe3
nkmry <yuuun.k16@gmail.com>
2022-02-19 15:45:28 +0900

    [Add]Chapter8

--- JavaIntro/src/chapter8/Main.java
+++ JavaIntro/src/chapter8/Main.java
@@ -8,9 +8,23 @@ public class Main {
 		h.hp = 100;
 		System.out.println("勇者" + h.name + "を生み出しました！");
 		
-		h.sit(5);
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
+		
+		Matango m2 = new Matango();
+		m2.hp = 48;
+		m2.suffix = 'B';
+		
+//		h.sit(5);
+//		h.slip();
+//		h.sit(25);
+//		h.run();
+		
 		h.slip();
-		h.sit(25);
+		m1.run();
+		m2.run();
 		h.run();
+		
 	}
 }
