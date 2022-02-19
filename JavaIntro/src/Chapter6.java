69819c4d6a207541ebbc59c61f5a864ab41f9f8a
nkmry <yuuun.k16@gmail.com>
2022-02-19 12:15:54 +0900

    [Add]Chapter6

---
+++ JavaIntro/src/Chapter6.java
@@ -0,0 +1,11 @@
+
+import comment.Zenhan;
+
+public class Chapter6 {
+	public static void main(String[] args) throws Exception{
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		comment.Kouhan.callDeae();
+		comment.Kouhan.showMondokoro();
+	}
+}
