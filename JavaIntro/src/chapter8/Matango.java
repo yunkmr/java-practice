3276e907e2217456b42cc5cde229fb1572d94013
nkmry <yuuun.k16@gmail.com>
2022-02-19 15:22:18 +0900

    [Fix]Chapter8

--- JavaIntro/src/chapter8/Matango.java
+++ JavaIntro/src/chapter8/Matango.java
@@ -3,27 +3,4 @@ package chapter8;
 public class Matango {
 	int hp;
 	final int LEVEL = 10;
-	
-	void sleep() {
-		this.hp = 100;
-		System.out.println(this.name + "は、眠って回復しました！");
-	}
-	
-	void sit(int sec) {
-		this.hp += sec;
-		System.out.println(this.name + "は、" + sec + "秒座った！");
-		System.out.println("HPが" + sec + "ポイント回復した.");
-	}
-	
-	void slip() {
-		this.hp -= 5;
-		System.out.println(this.name + "は、転んだ！");
-		System.out.println("５のダメージ！");
-	}
-	
-	void run() {
-		System.out.println(this.name + "は、逃げ出した！");
-		System.out.println("GAMEOVER");
-		System.out.println("最終HPは" + this.hp + "でした");
-	}
 }
