a1ef94a4f2421cbc1595bcaa94d0ba5acc9523c2
nkmry <yuuun.k16@gmail.com>
2022-02-20 10:18:48 +0900

    [Add]Chapter8

--- JavaIntro/src/chapter8/Cleric.java
+++ JavaIntro/src/chapter8/Cleric.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Cleric {
 	String name;
 	int hp = 50;
-	final int MAX_HP = 50;
+	static final int MAX_HP = 50;
 	int mp = 10;
-	final int MAX_MP = 10;
+	static final int MAX_MP = 10;
 	
 	void selfAid() {
 		System.out.println(this.name + "はセルフエイドを唱えた！");
@@ -21,12 +21,26 @@ public class Cleric {
 		
 	    Random rand = new Random();
 	    int recover = rand.nextInt(3) + sec;
-	    int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
+	    int recoverActual = Math.min(MAX_MP - this.mp, recover);
 	    
 	    mp += recoverActual;
 	    		
 	    System.out.println("MPが" + recoverActual + "回復した");
 	    
 	    return recoverActual;
+	}
+	
+	public Cleric(String name, int hp, int mp){
+		this.name = name;
+		this.hp = hp;
+		this.mp = mp;
+	}
+	
+	public Cleric(String name, int hp){
+		this(name, hp, Cleric.MAX_MP);
+	}
+	
+	public Cleric(String name){
+		this(name, Cleric.MAX_HP);
 	}
 }
