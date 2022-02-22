a10d2f7ebad7fb06e5aff23c5265bad9ec9259cd
nkmry <yuuun.k16@gmail.com>
2022-02-22 15:36:40 +0900

    [Fix]Chapter11

--- JavaIntro/src/chapter11/PoisonMatango.java
+++ JavaIntro/src/chapter11/PoisonMatango.java
@@ -1,5 +1,23 @@
 package chapter11;
 
-public class PoisonMatango {
+public class PoisonMatango extends Matango {
+	private int count = 5;
+	private int dmg;
 	
+	public PoisonMatango(char suffix) {
+		super(suffix);
+	}
+	
+	public void attack(Hero h) {
+		super.attack(h);
+		
+		if (this.count > 0) {
+			System.out.println("さらに毒の胞子をばらまいた");
+			dmg = h.getHp() / 5;
+			h.setHp(h.getHp() - dmg);
+			System.out.println(dmg + "ポイントのダメージをあたえた！");
+			this.count--;
+		}
+		
+	}
 }
