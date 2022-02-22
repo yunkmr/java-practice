58b8ec75e1bfd10b7ea3fe701d8c234893cf3a29
nkmry <yuuun.k16@gmail.com>
2022-02-22 17:23:32 +0900

    [Add]Chapter01

---
+++ JavaWcp_app/src/chapter01/chapter01.java
@@ -0,0 +1,9 @@
+package chapter01;
+
+public class chapter01 {
+	public static void main(String[] args) {
+		Human human = new Human();
+		Human.HumanLeg humanleg = human.new HumanLeg();
+		humanleg.kick();
+	}
+}
