4ec476b4c58014d8ccdb3844b98823174d495be9
nkmry <yuuun.k16@gmail.com>
2022-02-22 17:37:26 +0900

    [Add]Chapter02

---
+++ JavaWcp_app/src/chapter02/chapter02.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+public class chapter02 {
+	public static void main(String[] args) {
+		Dessert.APPLE.advertise();
+		Dessert.ICE_CREAM.advertise();
+		Dessert.CAKE.advertise();
+	}
+}
