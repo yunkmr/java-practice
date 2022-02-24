98cb90a36a278b48a10dd7ecff3fc03dfa002b32
nkmry <yuuun.k16@gmail.com>
2022-02-24 10:23:20 +0900

    [Add]Chapter04

---
+++ JavaWcp_app/src/chapter04/Fruit.java
@@ -0,0 +1,17 @@
+package chapter04;
+
+import java.math.BigDecimal;
+
+abstract class Fruit {
+	String name;
+	BigDecimal price;
+	
+	Fruit(String name, BigDecimal price){
+		this.name = name;
+		this.price = price;
+	}
+	
+	abstract protected String getName();
+	
+	abstract protected BigDecimal getPrice();
+}
