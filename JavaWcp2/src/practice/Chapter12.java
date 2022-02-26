1a0a2facfa02f72ab05b2ba6cac0e4763345afd3
nkmry <yuuun.k16@gmail.com>
2022-02-26 16:40:52 +0900

    [Fix]Chapter12

--- JavaWcp2/src/practice/Chapter12.java
+++ JavaWcp2/src/practice/Chapter12.java
@@ -2,10 +2,10 @@ package practice;
 
 public class Chapter12 {
     public static void main(String[] args) {
-        NoCapsuleMan man = new NoCapsuleMan("小林", 32);
-        System.out.println("名前は" + man.name +  "です。");
-        System.out.println("年齢は" + man.age + "です。");
-        man.age = 33;
-        System.out.println("誕生日を迎えたので、年齢が" + man.age + "になりました。");
+        CapsuleMan man = new CapsuleMan("小林", 32);
+        System.out.println("名前は" + man.getName() +  "です。");
+        System.out.println("年齢は" + man.getAge() + "です。");
+        man.setAge(33);
+        System.out.println("誕生日を迎えたので、年齢が" + man.getAge() + "になりました。");
     }
 }
