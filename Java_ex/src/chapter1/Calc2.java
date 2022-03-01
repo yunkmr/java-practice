41fd7cc412c923c5c8ae849c05759c428c294cb9
nkmry <yuuun.k16@gmail.com>
2022-03-01 19:13:21 +0900

    [Add]演習3

---
+++ Java_ex/src/chapter1/Calc2.java
@@ -0,0 +1,27 @@
+package chapter1;
+
+public class Calc2 {
+	public static void main(String[] args) {
+		int data[] = {32, 27, 80, 120, 76, 18, 212, 320, 53, 44, 61, 420};
+		
+		int maxValue = data[0];
+		int maxMonth = 1;
+		int minValue = data[0];
+		int minMonth = 1;
+		
+		for (int i = 1; i < data.length; i++) {
+			if (maxValue < data[i]) {
+				maxValue = data[i];
+				maxMonth = i + 1;
+			}
+			
+			if (minValue > data[i]) {
+				minValue = data[i];
+				minMonth = i + 1;
+			}
+		}
+		
+		System.out.println("最大値 >> " + maxValue + "個\t" + maxMonth + "月");
+		System.out.println("最小値 >> " + minValue + "個\t" + minMonth + "月");
+	}
+}
