3160b118f4964b971f7fdfbae4a1a7732d2674b2
nkmry <yuuun.k16@gmail.com>
2022-03-01 18:54:47 +0900

    [Add]演習1

---
+++ Java_ex/src/chapter1/DispArray.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+public class DispArray {
+	public static void main(String[] args) {
+		int data[] = {32, 27, 80, 120, 76, 18, 212, 320, 53, 44, 61, 420};
+		
+		for (int i = 0; i < data.length; i++) {
+			int month = Math.round((float) data[i] / 10);
+			
+			System.out.print((i + 1) + "月     |");
+			for (int j = 0; j < month; j++) {
+				System.out.print("※");
+			}
+			System.out.println();
+		}
+	}
+}
