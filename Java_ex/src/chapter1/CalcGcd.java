82b08c947481e7a4fad0a74db1dc7fa2cf31dcdd
nkmry <yuuun.k16@gmail.com>
2022-03-02 16:22:52 +0900

    [Add]演習5

---
+++ Java_ex/src/chapter1/CalcGcd.java
@@ -0,0 +1,42 @@
+package chapter1;
+
+public class CalcGcd {
+	public static void main(String[] args) {
+		
+		int m;
+		int n;
+		int a;
+		int b;
+		int r;
+		
+		if (args.length == 2) {
+			try{
+				m = Integer.parseInt(args[0]);
+				n = Integer.parseInt(args[1]);
+				
+				if( m > n ) {
+					a = m;
+					b = n;
+				} else {
+					a = n;
+					b = m;
+				}
+				
+				r = a % b;
+				while (r != 0) {
+					a = b;
+					b = r;
+					r = a % b;
+				}
+				
+				System.out.println(m + " と " + n + " 最大公約数は " + b + " です。");
+				
+			} catch( NumberFormatException e) {
+				System.out.println("引数には整数を指定してください。");
+			}
+		} else {
+			System.out.println("引数は２個指定してください。");
+		}
+		
+	}
+}
